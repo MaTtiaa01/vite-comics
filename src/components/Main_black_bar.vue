fd1cfc34237fddf5a382b121fee37fe7701c5a38
MaTtiaa01 <mattia2001@live.it>
2022-11-10 16:57:22 +0100

    add dynamic shop icons

--- src/components/Main_black_bar.vue
+++ src/components/Main_black_bar.vue
@@ -5,7 +5,21 @@ export default {
 </script>
 
 <template>
-    <section class="black_bar">
-        <h2> Content goes here</h2>
+
+    <section class="black_bar ">
+        <div class="container">
+            <h2> Content goes here</h2>
+        </div>
     </section>
+
 </template>
+
+<style lang="scss" scoped>
+@use "../assets/scss/Variables.scss" as *;
+
+section {
+    background-color: $dark;
+    color: $lighter;
+    padding: 2rem 0;
+}
+</style>
--- src/components/Main_icons_shop.vue
+++ src/components/Main_icons_shop.vue
@@ -1,6 +1,33 @@
 <script>
 export default {
-    name: "Main_icons_shop"
+    name: "Main_icons_shop",
+    data() {
+        return {
+            products: [
+                {
+                    product: "digital comics",
+                    img: "buy-comics-digital-comics.png"
+                },
+                {
+                    product: "digital comics",
+                    img: "buy-comics-merchandise.png"
+                },
+                {
+                    product: "digital comics",
+                    img: "buy-comics-shop-locator.png"
+                },
+                {
+                    product: "digital comics",
+                    img: "buy-comics-subscription.png"
+                },
+                {
+                    product: "digital comics",
+                    img: ""
+                },
+            ],
+        }
+    }
+
 }
 
 </script>
@@ -8,12 +35,17 @@ export default {
     <section class="icons_shop">
         <div class="container">
             <div class="row row-cols-5">
-                <div class="col">ba</div>
-                <div class="col">ba</div>
-                <div class="col">ba</div>
-                <div class="col">ba</div>
-                <div class="col">ba</div>
+                <div class="col" v-for="product, i in products">
+                    <div class="product_image">
+                        <img :src="`../assets/img/${product.img}`" alt="">
+                    </div>
+                    <div class="product_name">{{ product.product }}</div>
+                </div>
             </div>
         </div>
     </section>
 </template>
+
+<style>
+
+</style>
