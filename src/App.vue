89df016bb37aa532162314ae1c35155a3c09b9b0
MaTtiaa01 <mattia2001@live.it>
2022-11-10 15:14:22 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+  name: "App",
+  components: {
+
+  },
+}
+</script>
+
+</template>
+<header id="site_header">
+    <div class="logo">
+      <img src="" class="" alt="">
+    </div>
+    <div class="navbar">
+      <ul>
+        <li>
+          <a href="#"></a>
+        </li>
+      </ul>
+    </div>
+</header>
+  <!-- /#site_header -->
+<main id="site_main"></main>
+  <!-- /#site_main -->
+<footer id="site_footer"></footer>
+  <!-- /#site_footer -->
+</template>
+
+
+<style>
+
+</style>
