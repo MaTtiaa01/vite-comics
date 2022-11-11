ff3204b95ff27dec9a91e3d11141e29cacd5230b
MaTtiaa01 <mattia2001@live.it>
2022-11-11 17:01:51 +0100

    style footer_top

--- src/components/Footer_top.vue
+++ src/components/Footer_top.vue
@@ -9,10 +9,44 @@ export default {
         <div class="container">
             <div class="row row-cols-2">
                 <div class="col">
-                    <div class="row row-cols-3">
-                        <div class="col"></div>
-                        <div class="col"></div>
-                        <div class="col"></div>
+                    <div class="row left gx-4">
+                        <div class="col-2 ">
+                            <h4 class="text-uppercase">Dc comics</h4>
+                            <ul>
+                                <li><a href="#">Movies</a></li>
+                                <li><a href="#">Movies</a></li>
+                                <li><a href="#">Movies</a></li>
+                                <li><a href="#">Movies</a></li>
+                                <li><a href="#">Movies</a></li>
+                                <li><a href="#">Movies</a></li>
+                                <li><a href="#">Movies</a></li>
+                            </ul>
+                        </div>
+                        <div class="col-2 ">
+                            <h4 class="text-uppercase">dc</h4>
+                            <ul>
+                                <li><a href="#">Movies</a></li>
+                                <li><a href="#">Movies</a></li>
+                                <li><a href="#">Movies</a></li>
+                                <li><a href="#">Movies</a></li>
+                                <li><a href="#">Movies</a></li>
+                                <li><a href="#">Movies</a></li>
+                                <li><a href="#">Movies</a></li>
+                                <li><a href="#">Movies</a></li>
+                                <li><a href="#">Movies</a></li>
+                                <li><a href="#">Movies</a></li>
+                            </ul>
+                        </div>
+                        <div class="col-2 ">
+                            <h4 class="text-uppercase">sites </h4>
+                            <ul>
+                                <li><a href="#">Movies</a></li>
+                                <li><a href="#">Movies</a></li>
+                                <li><a href="#">Movies</a></li>
+                                <li><a href="#">Movies</a></li>
+                                <li><a href="#">Movies</a></li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
                 <div class="col dc_background">
@@ -23,8 +57,33 @@ export default {
     </section>
 </template>
 
+
 <style lang="scss" scoped>
+@use "../assets/scss/Variables.scss" as *;
+
+.footer_top {
+    background-image: url(../assets/img/footer-bg.jpg);
+}
+
+h4 {
+    color: $lighter;
+    margin: 1rem 0;
+}
+
+ul {
+    list-style: none;
+    margin-bottom: 1rem;
+
+
+    a {
+        text-decoration: none;
+        color: $light-dark;
+    }
+}
+
 .dc_background {
     background-image: url(../assets/img/dc-logo-bg.png);
+    background-repeat: no-repeat;
+    background-position: center;
 }
 </style>
