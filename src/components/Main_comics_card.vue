eeeb736ef929e99d4f1a7b08c8ad3d06c8f591e2
MaTtiaa01 <mattia2001@live.it>
2022-11-11 17:09:12 +0100

    add button load more

--- src/components/Main_comics_card.vue
+++ src/components/Main_comics_card.vue
@@ -23,13 +23,32 @@ export default {
 </script>
 
 <template>
-    <div class="container">
-        <div class="row row-cols-2 row-cols-sm-4 row-cols-md-6">
-            <Card_comic v-for="comic in comics" :image="getImageUrl(comic.thumb)" :title="comic.series"></Card_comic>
+    <div class="comics_cards">
+
+        <div class="container text-center py-4">
+            <div class="row row-cols-2 row-cols-sm-4 row-cols-md-6">
+                <Card_comic v-for="comic in comics" :image="getImageUrl(comic.thumb)" :title="comic.series">
+                </Card_comic>
+            </div>
+            <button class="text-uppercase">load more</button>
         </div>
     </div>
 </template>
 
 <style lang="scss" scoped>
+@use "../assets/scss/Variables.scss" as *;
 
+.comics_cards {
+    background-color: $dark;
+}
+
+button {
+    color: $lighter;
+    background-color: $primary;
+    padding: 0.25rem 1.5rem;
+    font-weight: 400;
+    font-size: 10px;
+    border: 1px solid $primary;
+    margin-top: 2rem;
+}
 </style>
